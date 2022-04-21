b39988e67e3a2aad46b6dabbf2d9023900fdb23d
Darío Scattolini <dario.scattolini@gmail.com>
2022-04-21 14:46:52 +0200

    fix index page data line breaks

--- data/pages/index.tsx
+++ data/pages/index.tsx
@@ -7,9 +7,9 @@ const metaTitle = translate({
 });
 
 const metaDescription = translate({
-  message: `
-    Personal website of Darío Scattolini - Junior web developer - Articles on
-    programming, blog posts, portfolio and more...
+  message: `\
+    Personal website of Darío Scattolini - Junior web developer - Articles on \
+    programming, blog posts, portfolio and more...\
   `,
   description: 'Home Page meta description',
   id: 'index.meta.description'
@@ -28,13 +28,13 @@ const aboutTitle = translate({
 });
 
 const aboutContent = translate({
-  message: `
-    I'm a junior web developer (and philosopher!) with a focus on the 
-    front-end side of the JavaScript technology stack. I started coding in 
-    2019, and soon became quite skilled in JavaScript and Angular. Working 
-    as a freelancer I also had the chance to get involved with PrestaShop, 
-    PHP and Symfony. I'm 32 years old, from Argentina, and I currently live
-    in Barcelona. 
+  message: `\
+    I'm a junior web developer (and philosopher!) with a focus on the \
+    front-end side of the JavaScript technology stack. I started coding in \
+    2019, and soon became quite skilled in JavaScript and Angular. Working \
+    as a freelancer I also had the chance to get involved with PrestaShop, \
+    PHP and Symfony. I'm 32 years old, from Argentina, and I currently live \
+    in Barcelona.\
   `,
   description: 'About content in Home Page',
   id: 'index.about.content'
