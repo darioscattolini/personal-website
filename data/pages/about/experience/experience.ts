7f12967a1bcc4bbe879d8191d58fca94ffee29e2
Darío Scattolini <dario.scattolini@gmail.com>
2022-06-08 15:36:28 +0200

    remove unfinished About sections

--- data/pages/about/experience/experience.ts
+++ data/pages/about/experience/experience.ts
@@ -1,17 +1,3 @@
-import { ExperienceLandmark } from '../../../models';
 import { header } from './header';
-import { conicet } from './landmarks/conicet';
-import { itAcademy } from './landmarks/it-academy';
-import { majorelModerator } from './landmarks/majorel-moderator';
-import { majorelQA } from './landmarks/majorel-qa';
-import { philosophy } from './landmarks/philosophy';
 
-const landmarks: ExperienceLandmark[] = [
-  majorelQA,
-  majorelModerator,
-  itAcademy,
-  conicet,
-  philosophy
-];
-
-export const experienceSectionData = { header, landmarks };
+export const experienceSectionData = { header };
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,6 @@ import Layout from '@theme/Layout';
 import Header from '@site/src/components/about/Header/Header';
 import Section from '@site/src/components/about/Section/Section';
 import Skills from '@site/src/components/about/Skills/Skills';
-import Experience from '../components/about/Experience/Experience';
 import { aboutData as data } from '@site/data/data';
 
 export default function About(): JSX.Element {
@@ -21,14 +20,6 @@ export default function About(): JSX.Element {
       <main>
         <Section { ...data.skillsSectionData.header }>
           <Skills skills={ data.skillsSectionData.skills } />
-        </Section>
-
-        <Section { ...data.portfolioSectionData.header } dark>
-          <div></div>
-        </Section>
-
-        <Section { ...data.experienceSectionData.header }>
-          <Experience landmarks={ data.experienceSectionData.landmarks }/>
         </Section>
       </main>
     </Layout>
